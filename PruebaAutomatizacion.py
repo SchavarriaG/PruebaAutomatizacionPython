e7b0fbfef4aae0210b212c2363f6e3a1ce389b0a
SchavarriaG <schavarriag@unal.edu.co>
2023-05-22 23:51:54 -0500

    Update PruebaAutomatizacion.py
    Ciclo 4 ready

--- PruebaAutomatizacion.py
+++ PruebaAutomatizacion.py
@@ -10,18 +10,23 @@ import time
 
 def buscarPregunta(pregunta):
     if(pregunta == "¿Cuál de estos números es múltiplo de 5?"):
-        return "Es la pregunta 1."
+        return "Es la pregunta múltiplos."
     elif(len(pregunta) > 17 and pregunta == "Indique cuantos "+ pregunta[16] +" hay en la siguiente secuencia:" ):
-        return "Es la pregunta 2."
+        return "Es la pregunta de buscar emojis."
     elif(pregunta == "Indique la fecha que corresponde a 104 dias contados desde antes de : " + datetime.now().strftime('23/%m/%Y')):#%d
-        return "Es la pregunta 3."
+        return "Es la pregunta de buscar fecha."
     elif(pregunta == "Complete la siguiente operación matemática:"):
-        return "Es la pregunta 4."
+        return "Es la pregunta de operación matemática."
+    elif(pregunta == "Escriba 199 veces la letra 'X'"):
+        return "Es la pregunta de escribir la X varias veces"
     else:
         return"No se encontró."
     
 def resolverPreguntaUno(pregunta):
     return null
+
+def clickBotonEnviar(driver):
+    driver.find_element_by_xpath("/html/body/div[2]/form/div[2]/button").send_keys(Keys.ENTER)
 
 #options = Options()
 #options.add_argument("--headless")
@@ -37,7 +42,7 @@ password = driver.find_element_by_xpath("/html/body/div/div[2]/form/input[2]")
 password.send_keys("10df2f32286b7120MS00LTE5MDExNg==30e0c83e6c29f1c3")
 password.send_keys(Keys.ENTER)
 
-
+##CICLO 1
 #ciclos =  driver.find_element(By.CLASS_NAME, "text-xl text-center text-green-500")
 ciclos = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/p[2]")
 print(ciclos.text)
@@ -63,11 +68,11 @@ animalsEmojiGroup = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/
 inputButton = driver.find_element(By.XPATH, "/html/body/div[2]/form/div[1]/div[2]/input")
 countEmojis = 0
 for emoji in animalsEmojiGroup.text:
-    if emoji == pregunta1.text[16]:
+    if emoji == pregunta2.text[16]:
         countEmojis += 1
 inputButton.send_keys(str(countEmojis)) 
 #inputButton.send_keys(Keys.ENTER)
-driver.find_element_by_xpath("/html/body/div[2]/form/div[2]/button").send_keys(Keys.ENTER)
+clickBotonEnviar(driver)
 
 ###CICLO2
 ciclos = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/p[2]")
@@ -95,7 +100,7 @@ for elemento in lista:
         elemento.click()
 
 #Enter al botón Enviar
-driver.find_element_by_xpath("/html/body/div[2]/form/div[2]/button").send_keys(Keys.ENTER)
+clickBotonEnviar(driver)
 
 ###CICLO3
 ciclos = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/p[2]")
@@ -125,7 +130,7 @@ for elemento in lista:
         elemento.click()
 
 #Enter al botón Enviar
-driver.find_element_by_xpath("/html/body/div[2]/form/div[2]/button").send_keys(Keys.ENTER)
+clickBotonEnviar(driver)
 
 ###CICLO4
 ciclos = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/p[2]")
@@ -136,17 +141,148 @@ print(buscarPregunta(pregunta1.text))
 print(buscarPregunta(pregunta2.text))
 
 #PREGUNTA 1
-animalsEmojiGroup = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[2]/div/p[2]")
+animalsEmojiGroup = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[1]/div/p[2]")
 #print(str(len(animalsEmojiGroup.text)))
 #inputButton = driver.find_element(By.CLASS_NAME, "border-2 border-black rounded-sm p-2")
-inputButton = driver.find_element(By.XPATH, "/html/body/div[2]/form/div[1]/div[2]/input")
+inputButton = driver.find_element(By.XPATH, "/html/body/div[2]/form/div[1]/div[1]/input")
 countEmojis = 0
 for emoji in animalsEmojiGroup.text:
     if emoji == pregunta1.text[16]:
         countEmojis += 1
 inputButton.send_keys(str(countEmojis)) 
-#inputButton.send_keys(Keys.ENTER)
-driver.find_element_by_xpath("/html/body/div[2]/form/div[2]/button").send_keys(Keys.ENTER)
+
+#PREGUNTA 2
+if driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[1]//p[2]").text[:2] == "=?":
+    print("Pregunta de suma tipo 1")
+else: 
+    print("Pregunta de suma tipo 2")
+    x = eval(driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[2]/p[2]").text)
+    selectButton = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[2]/select")
+    options = selectButton.find_elements_by_tag_name("option")
+    
+    print("la suma es "+ str(x))
+    for option in options:
+        if option.get_attribute("value") == str(x):
+            option.click()
+    
+
+#Enter al botón Enviar
+clickBotonEnviar(driver)
+
+###CICLO5
+ciclos = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/p[2]")
+print(ciclos.text)
+pregunta1 = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[1]//p")
+pregunta2 = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[2]//p")
+print(buscarPregunta(pregunta1.text))
+print(pregunta1.text)
+print(buscarPregunta(pregunta2.text))
+print(pregunta2.text)
+#PREGUNTA 1
+
+
+#PREGUNTA 2
+
+    
+
+#Enter al botón Enviar
+clickBotonEnviar(driver)
+
+
+####CICLO6
+#ciclos = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/p[2]")
+#print(ciclos.text)
+#pregunta1 = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[1]//p")
+#pregunta2 = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[2]//p")
+#print(buscarPregunta(pregunta1.text))
+#print(pregunta1.text)
+#print(buscarPregunta(pregunta2.text))
+#print(pregunta2.text)
+##PREGUNTA 1
+#
+#
+##PREGUNTA 2
+#
+#    
+#
+##Enter al botón Enviar
+#clickBotonEnviar(driver)
+#
+####CICLO7
+#ciclos = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/p[2]")
+#print(ciclos.text)
+#pregunta1 = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[1]//p")
+#pregunta2 = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[2]//p")
+#print(buscarPregunta(pregunta1.text))
+#print(pregunta1.text)
+#print(buscarPregunta(pregunta2.text))
+#print(pregunta2.text)
+##PREGUNTA 1
+#
+#
+##PREGUNTA 2
+#
+#    
+#
+##Enter al botón Enviar
+#clickBotonEnviar(driver)
+#
+####CICLO8
+#ciclos = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/p[2]")
+#print(ciclos.text)
+#pregunta1 = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[1]//p")
+#pregunta2 = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[2]//p")
+#print(buscarPregunta(pregunta1.text))
+#print(pregunta1.text)
+#print(buscarPregunta(pregunta2.text))
+#print(pregunta2.text)
+##PREGUNTA 1
+#
+#
+##PREGUNTA 2
+#
+#    
+#
+##Enter al botón Enviar
+#clickBotonEnviar(driver)
+#
+####CICLO9
+#ciclos = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/p[2]")
+#print(ciclos.text)
+#pregunta1 = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[1]//p")
+#pregunta2 = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[2]//p")
+#print(buscarPregunta(pregunta1.text))
+#print(pregunta1.text)
+#print(buscarPregunta(pregunta2.text))
+#print(pregunta2.text)
+##PREGUNTA 1
+#
+#
+##PREGUNTA 2
+#
+#    
+#
+##Enter al botón Enviar
+#clickBotonEnviar(driver)
+#
+####CICLO10
+#ciclos = driver.find_element_by_xpath("/html/body/div[2]/div/div/div/p[2]")
+#print(ciclos.text)
+#pregunta1 = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[1]//p")
+#pregunta2 = driver.find_element_by_xpath("/html/body/div[2]/form/div[1]/div[2]//p")
+#print(buscarPregunta(pregunta1.text))
+#print(pregunta1.text)
+#print(buscarPregunta(pregunta2.text))
+#print(pregunta2.text)
+##PREGUNTA 1
+#
+#
+##PREGUNTA 2
+#
+#    
+#
+##Enter al botón Enviar
+#clickBotonEnviar(driver)
 
 time.sleep(100)
 
